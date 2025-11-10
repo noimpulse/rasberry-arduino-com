75dad9d02bbf887419b02c985b3b1a5a40b03860
noimpulse <sashasergeev667@gmail.com>
2025-11-10 10:26:19 +0300

    Добавил json ответ

--- rasbery_py.py
+++ rasbery_py.py
@@ -10,6 +10,8 @@ CONFIRM = 0x00
 ERR_TX = 0x01
 ERR_ACK = 0x02
 ERR_ADDR = 0x03
+ERR_STM = 0x04
+
 
 # Отправка команды
 def send_command_to_stm32(zone, command):
@@ -19,6 +21,8 @@ def send_command_to_stm32(zone, command):
     :param command: номер команды (байт)
     """
     try:
+
+        start_time = time.time()
         # Формируем пакет (2 байта)
         packet = struct.pack('BB', zone, command)
         ser.write(packet)
@@ -29,11 +33,22 @@ def send_command_to_stm32(zone, command):
         response = ser.read(1)
         if not response:
             print("⚠ Нет ответа от STM32")
-            return
+            return {
+              "number_of_command": command,
+              "error_code": ERR_STM,
+              "command_status ": "Failed", 
+              "execution_time_ms": 0
+            }
 
         code = response[0]
         if code == CONFIRM:
             print("✅ Команда подтверждена STM32")
+            return {
+              "number_of_command": command,
+              "error_code": CONFIRM,
+              "command_status ": "OK", 
+              "execution_time_ms": (time.time() - start_time) * 1000
+            }
         elif code == ERR_TX:
             print("❌ Ошибка передачи на Arduino")
         elif code == ERR_ACK:
@@ -42,9 +57,21 @@ def send_command_to_stm32(zone, command):
             print("❌ Неверный адрес Arduino")
         else:
             print(f"⚠ Неизвестный код ответа: {hex(code)}")
+        return {
+          "number_of_command": command,
+          "error_code": code,
+          "command_status ": "Failed", 
+          "execution_time_ms": (time.time() - start_time) * 1000
+        }
 
     except Exception as e:
         print(f"Ошибка при передаче: {e}")
+        return {
+          "number_of_command": command,
+          "error_code": ERR_STM,
+          "command_status ": "Failed", 
+          "execution_time_ms": 0
+        }
 
 # Пример использования
 if __name__ == "__main__":
